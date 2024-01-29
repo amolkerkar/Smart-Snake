4a50a1d7a8eccb61a003cecf3957c7ec059377d4
amol.kerkar <amolkerkar2@gmail.com>
2024-01-29 18:42:11 -0500

    Init MainGame python file

---
+++ Main_Game.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+from collections import namedtuple
+from enum import Enums
+
+pygame.init()
+
+Class MainGame:
+    def __init__(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    game = MainGame()
+    
+    while True:
+        pass
+
+        
+    print('Your score', score)
+    
+    pygame.quit()
